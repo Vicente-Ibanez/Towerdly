4a62d609185bf1ef9854287a0342060f242093fb
Vicente Ibanez <vincentibanez2001@gmail.com>
2024-01-20 15:35:39 -0500

    Camera Movements

--- Scripts/camera_movement.gd
+++ Scripts/camera_movement.gd
@@ -1,4 +1,9 @@
-extends Node
+extends Camera2D
+
+const MOVE_SPEED = 750
 
 func _process(delta):
-	pass
+	if Input.is_action_pressed("move_camera_left"):
+		global_position += Vector2.LEFT * delta * MOVE_SPEED
+	elif Input.is_action_pressed("move_camera_right"):
+		global_position += Vector2.RIGHT * delta * MOVE_SPEED
