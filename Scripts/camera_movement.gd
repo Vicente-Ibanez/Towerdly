2e580832be2bc50aba504ff111f5f66d40b58f62
Vicente Ibanez <86447257+Vicente-Ibanez@users.noreply.github.com>
2024-01-23 20:30:04 -0500

    Adding max scroll

--- Scripts/camera_movement.gd
+++ Scripts/camera_movement.gd
@@ -1,9 +1,16 @@
 extends Camera2D
 
 const MOVE_SPEED = 750
+var left_limit = 600
+var right_limit = 4750
+
 
 func _process(delta):
 	if Input.is_action_pressed("move_camera_left"):
-		global_position += Vector2.LEFT * delta * MOVE_SPEED
+		print_debug("left", global_position.x)
+		if(global_position.x >= left_limit):
+			global_position += Vector2.LEFT * delta * MOVE_SPEED
 	elif Input.is_action_pressed("move_camera_right"):
-		global_position += Vector2.RIGHT * delta * MOVE_SPEED
+		if(global_position.x <= right_limit):
+			global_position += Vector2.RIGHT * delta * MOVE_SPEED
+			print_debug("right", global_position.x)
